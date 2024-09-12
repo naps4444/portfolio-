2342f5c7ac80f8f18584beee6fd26654acab3e79
naps4444 <uchemadunmerengwa@gmail.com>
2024-09-12 11:32:20 +0100

    corrected missing link

--- src/Components/Section3.jsx
+++ src/Components/Section3.jsx
@@ -51,7 +51,7 @@ const Section3 = () => {
                 <h1 className='text-lg font-semibold'>TaskDuty</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros.</p>
 
-                <a href='https://taskduty-vs1.vercel.app/' className='flex w-[125px] items-center gap-2  hover:border-b-2 hover:border-[#5E3BEE]'>
+                <a href='taskduty-pi.vercel.app/' className='flex w-[125px] items-center gap-2  hover:border-b-2 hover:border-[#5E3BEE]'>
                     <p>View website</p>
                     <img src={arrow} alt="arrow icon" />
                 </a>
